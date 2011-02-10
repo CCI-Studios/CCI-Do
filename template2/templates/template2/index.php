0140e32faf8c76d050acfa197b0c1d0d5dc5c277
Jonathan Bennett <jbennett@ccistudios.com>
2011-02-10 11:21:37 -0500

    Added menu/test php to the index and added google analytics placeholder

--- template2/templates/template2/index.php
+++ template2/templates/template2/index.php
@@ -1,4 +1,12 @@
 <!DOCTYPE html>
+<?php
+$menu = JSite::getMenu();
+if ($menu)
+    $menu = $menu->getActive();
+if ($menu)
+    $menu = $menu->alias;
+$testing = "true";
+?>
 <html>
 <head>
 	<jdoc:include type="head" />
@@ -7,17 +15,36 @@
 	<link href="/templates/template2/css/template.css" rel="stylesheet" /> 
 	<!-- scripts -->
 	<!-- misc -->
+	
+	<?php if ($testing): ?>
+		<meta name="robots" content="nofollow, noindex" />
+	<?php endif; ?>
 </head>
 
-<body>
+<body class="<?php echo $menu?>"><div>
+	<?php if (!$testing): ?>
+		google analytics
+	<?php endif; ?>
+	
 	<div id="wrapper">
-		<div id="body"><div><div>
+		<div id="body"><div><div><div>
 			<jdoc:include type="component" />
-		</div></div></div>
-		<div id="siteby">Site By CCI Studios</div>
+		</div></div></div></div>
+		
+		<div id="siteby">
+			<a href="http://www.ccistudios.com" target="_blank">
+				<img src="/templates/template2/images/cci.png" />
+			</a>
+		</div>
+		
 		<div id="footer">
 			<jdoc:include type="modules" name="footer" style="xhtml" />
 		</div>
 	</div>	
-</body>
+	
+	<div class="hidden">
+		<jdoc:include type="modules" name="hidden" style="raw" />
+	</div>
+
+</div></body>
 </html>
