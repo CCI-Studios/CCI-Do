6dfec460f8bc5f5d9e4366e78e70bb57a86a1b8f
dmurray <dmurray@ccistudios.com>
2011-02-14 15:45:42 -0500

    Updated Template

---
+++ template1/templates/template1/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!--
+	<jdoc:include type="head" />
+	-->
+	
+	<link rel="stylesheet" href="/templates/template1/css/template.css" />
+		
+</head>
+
+<body>
+	
+	<div id="wrapper">
+		<div id="top">
+			<jdoc:include type="modules" name="top" style="xhtml" />
+			<div class="clear"></div>			
+		</div>
+		
+		<div id="body">
+			<div class="clear"></div>
+		</div>
+		
+		<div id="siteby">
+			<img src="/templates/template1/images/site-by.png" width="11" height="92" alt="Site By" />
+		</div>
+		
+		<div id="menu">
+			<jdoc:include type="modules" name="menu" style="xhtml" />
+			<div class="clear"></div>
+		</div>
+	</div>
+	
+</body>
+</html>
