4207401a203600b4c0807a54cbc3b608997c2239
dmurray <dmurray@ccistudios.com>
2011-02-15 16:35:45 -0500

    Darren's Completion of CCI Do - Template 1 More additions to be done by Jon (i.e. sidebar header, sidebar photos, main picture changes)

--- template1/templates/template1/index.php
+++ template1/templates/template1/index.php
@@ -1,29 +1,43 @@
 <!DOCTYPE html>
+<?php
+$menu = JSite::getMenu();
+if ($menu)
+    $menu = $menu->getActive();
+if ($menu)
+    $menu = $menu->alias;
+$testing = "true";
+?>
 <html>
 <head>
-	<!--
 	<jdoc:include type="head" />
-	-->
 	
 	<link rel="stylesheet" href="/templates/template1/css/template.css" />
-		
+	
 </head>
 
-<body>
+<body class="<?php echo $menu?>">
 	
 	<div id="wrapper">
 		<div id="top">
 			<jdoc:include type="modules" name="top" style="xhtml" />
-			<div class="clear"></div>			
+			<div class="clear"></div>
 		</div>
 		
-		<div id="body">
-			<jdoc:include type="component" />
+		<div id="body">	
+			<div id="inner">
+				<div id="sidebar">
+					<div class="content">
+						<jdoc:include type="component" />
+					</div>
+				</div>
+			</div>
 			<div class="clear"></div>
 		</div>
 		
 		<div id="siteby">
+			<a href="http://www.ccistudios.com" target="_blank">
 			<img src="/templates/template1/images/site-by.png" width="11" height="92" alt="Site By" />
+			</a>
 		</div>
 		
 		<div id="menu">
