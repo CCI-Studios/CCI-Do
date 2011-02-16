05e932d18cd07c6ecc0d5dd620ec397d5189f7f0
Jonathan Bennett <jbennett@ccistudios.com>
2011-02-16 08:23:23 -0500

    Updated symlink function, added Nooku symlink, updated $root to by dynamic

--- public_html/link.php
+++ public_html/link.php
@@ -1,19 +1,39 @@
 <?php 
-
+ 
 function _symlink($source, $destination, $path, $prefix = '') {
-	if ($prefix) $prefix .= '/';
-	
-	shell_exec("rm ".$destination.$path);
-	shell_exec("ln -s ".$source.$prefix.$path.' '.$destination.$path);
-	echo $source.$prefix.$path.' -> '.$destination.$path.'<br/>';
+    if ($prefix) $prefix .= '/';
+     
+    shell_exec("rm ".$destination.$path);
+    shell_exec("ln -s ".$source.$prefix.$path.' '.$destination.$path);
+    echo $source.$prefix.$path."\t".$destination.$path."<br/>\n";
 }
-
-$root = "~/subdomains/dev/";
-$site	= $root."public_html/";
-$t1		= $root."template1/";
-$t2		= $root."template2/";
-$t3		= $root."template3/";
-
+ 
+$root = dirname(getcwd()).'/';
+$site   = $root."public_html/";
+$t1     = $root."template1/";
+$t2     = $root."template2/";
+$t3     = $root."template3/";
+$com    = $root."com_ido/";
+$nooku= $root."nooku/";
+ 
 /* template shortcuts */
 _symlink($t1, $site, 'templates/template1');
 _symlink($t2, $site, 'templates/template2');
+ 
+/* component symlink */
+_symlink($com, $site, 'administrator/components/com_ido');
+ 
+ 
+/* nooku symlink */
+//shell_exec('cd '.$root.' && svn checkout http://svn2.assembla.com/svn/nooku-framework/trunk/code/ nooku'); // checkout Nooku
+// update Nooku
+_symlink($nooku, $site, 'administrator/components/com_default');
+_symlink($nooku, $site, 'administrator/modules/mod_default');
+_symlink($nooku, $site, 'libraries/koowa');
+_symlink($nooku, $site, 'media/com_default');
+_symlink($nooku, $site, 'media/lib_koowa');
+_symlink($nooku, $site, 'plugins/koowa');
+_symlink($nooku, $site, 'plugins/system/koowa.php');
+_symlink($nooku, $site, 'plugins/system/koowa.xml');
+_symlink($nooku, $site, 'components/com_default', 'site');
+_symlink($nooku, $site, 'modules/mod_default', 'site');
