5ad04e021671e8d7e2f28eefd9cbb78f405bcfd1
dmurray <dmurray@ccistudios.com>
2011-02-16 12:04:59 -0500

    Initialization of CCI Do - Template 3

---
+++ template3/templates/template3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+$menu = JSite::getMenu();
+if ($menu)
+    $menu = $menu->getActive();
+if ($menu)
+    $menu = $menu->alias;
+$testing = "true";
+?> 
+<html>
+
+<head>
+	<link rel="stylesheet" href="/templates/template3/css/template.css" />
+	
+</head>
+
+<body>
+		
+</body>
+</html>
