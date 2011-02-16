07839e756f0c3279ae5a979e9932d28f9e2061cb
dmurray <dmurray@ccistudios.com>
2011-02-16 16:34:16 -0500

    Initial build of CCI Do - Template 3 Lots of fixes needed!

--- template3/templates/template3/index.php
+++ template3/templates/template3/index.php
@@ -15,6 +15,19 @@ $testing = "true";
 </head>
 
 <body>
-		
+	<div id="main">
+		<div id="leftsidebar">
+			<img src="/templates/template3/images/left-sidebar-name.png" alt="page" />
+			<div id="rightsidebar">
+			</div>
+		</div>
+				<div id="inner">
+				</div>
+					<div id="menu">
+						<div id="menutitle">
+							<jdoc:include type="modules" name="menu" style="xhtml" />
+						</div>
+					</div>
+	</div>	
 </body>
 </html>
