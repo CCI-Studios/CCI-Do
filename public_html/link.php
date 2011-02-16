5ebee79f91b4731246845570559b7827ccc3e3d3
Jonathan Bennett <jbennett@ccistudios.com>
2011-02-16 09:50:50 -0500

    Removed bad characters

--- public_html/link.php
+++ public_html/link.php
@@ -1,29 +1,29 @@
-<?php 
- 
+<?php
+
 function _symlink($source, $destination, $path, $prefix = '') {
-    if ($prefix) $prefix .= '/';
-     
-    shell_exec("rm ".$destination.$path);
-    shell_exec("ln -s ".$source.$prefix.$path.' '.$destination.$path);
-    echo $source.$prefix.$path."\t".$destination.$path."<br/>\n";
+	if ($prefix) $prefix .= '/';
+
+	shell_exec("rm ".$destination.$path);   
+	shell_exec("ln -s ".$source.$prefix.$path.' '.$destination.$path);
+	echo $source.$prefix.$path."\t".$destination.$path."<br/>\n";
 }
- 
-$root = dirname(getcwd()).'/';
-$site   = $root."public_html/";
-$t1     = $root."template1/";
-$t2     = $root."template2/";
-$t3     = $root."template3/";
-$com    = $root."com_ido/";
-$nooku= $root."nooku/";
- 
+
+$root	= dirname(getcwd()).'/';
+$site	= $root."public_html/";
+$t1		= $root."template1/";
+$t2		= $root."template2/";
+$t3		= $root."template3/";
+$co		= $root."com_ido/";
+$nooku	= $root."nooku/";
+
 /* template shortcuts */
 _symlink($t1, $site, 'templates/template1');
 _symlink($t2, $site, 'templates/template2');
- 
+_symlink($t3, $site, 'templates/template3');
+
 /* component symlink */
 _symlink($com, $site, 'administrator/components/com_ido');
- 
- 
+
 /* nooku symlink */
 //shell_exec('cd '.$root.' && svn checkout http://svn2.assembla.com/svn/nooku-framework/trunk/code/ nooku'); // checkout Nooku
 // update Nooku
