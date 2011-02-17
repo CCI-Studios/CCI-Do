2e0bb3bd04a3ccf7b61b06fb3f329c61ea11cb52
dmurray <dmurray@ccistudios.com>
2011-02-17 17:03:21 -0500

    Updated CCI Do - Template #3 More work still needs to be done on home page which is different than all the other pages. Some further work needs to be done on remaining pages for consistancy.

--- template3/templates/template3/index.php
+++ template3/templates/template3/index.php
@@ -7,18 +7,48 @@ if ($menu)
     $menu = $menu->alias;
 $testing = "true";
 ?> 
+<head>
+	<link rel="stylesheet" href="/templates/template3/css/template.css" />
+</head>
 <html>
+<body>
 	<div id="main">
-		<div id="leftsidebar">
-			<img src="/templates/template3/images/left-sidebar-name.png" alt="page" />
-			<div id="rightsidebar"></div>
+		<div id="inner"><div>
+			component
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			<b>component</br>
+			</div></div>
+	</div>
+	
+	<div id="left-sidebar">
+		<div>
+		<img src="/templates/template3/images/left-sidebar-name.png" alt="page" />
 		</div>
 	</div>
-
-	<div id="inner"></div>
-
-	<div id="menu">
-		<div id="menutitle">
+	
+	<div id="right-sidebar">
+		<img src="/templates/template3/images/names.png" alt="names" />
+		<div>
+			October 15, 2011
 			<jdoc:include type="modules" name="menu" style="xhtml" />
 		</div>
 	</div>
