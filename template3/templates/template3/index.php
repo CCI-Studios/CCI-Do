3cdd0774a59718eabdac0e314e07be00603bb4d3
Jonathan Bennett <jbennett@ccistudios.com>
2011-02-17 08:42:14 -0500

    This is my beautiful index file, weee.

---
+++ template3/templates/template3/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<?php
+$menu = JSite::getMenu();
+if ($menu)
+    $menu = $menu->getActive();
+if ($menu)
+    $menu = $menu->alias;
+$testing = "true";
+?> 
+<html>
+ 
+<head>
+    <link rel="stylesheet" href="/templates/template3/css/template.css" />
+     
+</head>
+ 
+<body>
+    <div id="main">
+        <div id="leftsidebar">
+            <img src="/templates/template3/images/left-sidebar-name.png" alt="page" />
+            <div id="rightsidebar">
+            </div>
+        </div>
+	</div>
+</body>
+</html>
