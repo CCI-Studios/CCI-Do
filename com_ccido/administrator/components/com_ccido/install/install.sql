9659373f4a5b1cffd9ba773f6887768fc1e26532
Jonathan Bennett <jbennett@ccistudios.com>
2011-04-16 16:00:03 -0400

    Updated install sql

--- com_ccido/administrator/components/com_ccido/install/install.sql
+++ com_ccido/administrator/components/com_ccido/install/install.sql
@@ -1,20 +1,61 @@
-CREATE TABLE `jos_ccido_events` (
-	`ccido_event_id` SERIAL,
+CREATE TABLE `#__ccido_albums` (
+  `ccido_album_id` SERIAL,
+  `title` varchar(255) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`ccido_album_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-	`title` varchar(100) NOT NULL,
-	`description` text NOT NULL COMMENT '@Filter("html")',
-	`location` varchar(150) NOT NULL,
-	`directions` text NOT NULL COMMENT '@Filter("html")',
+CREATE TABLE `#__ccido_images` (
+  `ccido_image_id` SERIAL,
 
-	PRIMARY KEY (`cci_do_event_id`)
+  `title` varchar(255) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+
+  `ccido_album_id` bigint(20) UNSIGNED NOT NULL,
+  PRIMARY KEY (`ccido_image_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE `jos_ccido_messages` (
-  `ccido_message_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `#__ccido_events` (
+	`ccido_event_id` SERIAL,
+
+	`title` varchar(100) NOT NULL,
+	`location` varchar(150) NOT NULL,
+	`description` text NOT NULL COMMENT '@Filter("html")',
+	`directions` text NOT NULL COMMENT '@Filter("html")',
+
+	PRIMARY KEY (`ccido_event_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `#__ccido_links` (
+  `ccido_link_id` SERIAL,
+
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `description` text NOT NULL COMMENT '@Filter("html")',
+
+  PRIMARY KEY (`ccido_link_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `#__ccido_members` (
+  `ccido_member_id` SERIAL,
+
+  `name` varchar(255) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `bridesmaid` tinyint(1) NOT NULL,
+  `description` TEXT NOT NULL,
+
+  PRIMARY KEY (`ccido_member_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `#__ccido_messages` (
+  `ccido_message_id` SERIAL,
+
   `created_on` datetime NOT NULL,
   `author` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
+
   `description` text NOT NULL COMMENT '@Filter("html")',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`ccido_message_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
